402ea5e607f709a4035007bded35df4eb76f0518
FVXOU <juanc.guerrerop@unac.edu.co>
2024-12-26 03:48:59 -0500

    refactor: Refactor use case to use CrudService

--- src/main/java/com/permission_management/application/usecase/ModuleComponentUseCase.java
+++ src/main/java/com/permission_management/application/usecase/ModuleComponentUseCase.java
@@ -3,6 +3,7 @@ package com.permission_management.application.usecase;
 
 import com.permission_management.application.dto.request.RequestAssignAndRemoveBodyDTO;
 import com.permission_management.application.dto.common.GroupPermissionDTO;
+import com.permission_management.application.service.CrudService;
 import com.permission_management.domain.models.*;
 import com.permission_management.application.dto.common.ModuleComponentDTO;
 import com.permission_management.application.dto.response.ResponseHttpDTO;
@@ -23,86 +24,27 @@ import java.util.stream.Collectors;
 public class ModuleComponentUseCase {
     private final ModuleComponentGateway moduleComponentGateway;
     private final GroupPermissionGateway groupPermissionGateway;
+    private final CrudService crudService;
     private final ModelMapper modelMapper;
 
     public ResponseHttpDTO<ModuleComponentDTO> createComponent(ModuleComponent moduleComponent) {
-        try {
-
-            ModuleComponent savedComponent = moduleComponentGateway.save(moduleComponent);
-            ModuleComponentDTO ComponentDTO = modelMapper.map(savedComponent, ModuleComponentDTO.class);
-            return new ResponseHttpDTO<>("200", "Componente creado correctamente", ComponentDTO);
-
-        } catch (DataAccessException e) {
-            return new ResponseHttpDTO<>("500", "Error al guardar el componente: " + e.getMessage(), null);
-        } catch (Exception e) {
-            return new ResponseHttpDTO<>("500", "Ocurrió un error inesperado: " + e.getMessage(), null);
-        }
+       return crudService.create(moduleComponent, moduleComponentGateway, ModuleComponentDTO.class, "componente");
     }
 
     public ResponseHttpDTO<List<ModuleComponentDTO>> getAllComponents() {
-        try {
-            List<ModuleComponentDTO> allComponents = moduleComponentGateway.findAll().stream()
-                    .map(value -> modelMapper.map(value, ModuleComponentDTO.class)).collect(Collectors.toList());
-
-            if (allComponents.isEmpty()) {
-                return new ResponseHttpDTO<>("204", "No hay componentes disponibles", null);
-            }
-            return new ResponseHttpDTO<>("200", "Componentes obtenidos correctamente", allComponents);
-
-        } catch (DataAccessException e) {
-            return new ResponseHttpDTO<>("500", "Error al obtener los componentes: " + e.getMessage(), null);
-        } catch (Exception e) {
-            return new ResponseHttpDTO<>("500", "Ocurrió un error inesperado: " + e.getMessage(), null);
-        }
+       return crudService.getAllResource(moduleComponentGateway, ModuleComponentDTO.class, "componentes");
     }
 
     public ResponseHttpDTO<ModuleComponentDTO> getComponentById(UUID id) {
-
-        try {
-            return moduleComponentGateway.findById(id).map(
-                            moduleComponent -> {
-                                ModuleComponentDTO moduleComponentDTO = modelMapper.map(moduleComponent, ModuleComponentDTO.class);
-                                return new ResponseHttpDTO<>("200", "Componente obtenido correctamente", moduleComponentDTO);
-                            })
-                    .orElseGet(() -> new ResponseHttpDTO<>("404", "El componente no existe", null));
-        } catch (DataAccessException e) {
-            return new ResponseHttpDTO<>("500", "Error al obtener la informacion del componente: " + e.getMessage(), null);
-        } catch (Exception e) {
-            return new ResponseHttpDTO<>("500", "Ocurrió un error inesperado: " + e.getMessage(), null);
-        }
+        return crudService.getResourceById(id,moduleComponentGateway, ModuleComponentDTO.class, "componente");
     }
 
     public ResponseHttpDTO<String> deleteComponentById(UUID id) {
-        try {
-            return moduleComponentGateway.findById(id).map(moduleComponent -> {
-                moduleComponentGateway.deleteById(id);
-                return new ResponseHttpDTO<String>("200", "Componente eliminado correctamente", "OK");
-            }).orElseGet(() -> new ResponseHttpDTO<>("404", "El componente no existe", null));
-        } catch (DataAccessException e) {
-            return new ResponseHttpDTO<>("500", "Error al eliminar el componente: " + e.getMessage(), null);
-        } catch (Exception e) {
-            return new ResponseHttpDTO<>("500", "Ocurrió un error inesperado: " + e.getMessage(), null);
-        }
+        return crudService.deleteResourceById(id, moduleComponentGateway, "componente");
     }
 
     public ResponseHttpDTO<ModuleComponentDTO> updateComponentById(UUID id, ModuleComponentDTO moduleComponentDTO) {
-        try {
-            return moduleComponentGateway.findById(id).map(
-                    moduleComponent -> {
-                        modelMapper.getConfiguration().setSkipNullEnabled(true);
-                        modelMapper.map(moduleComponentDTO, moduleComponent);
-                        ModuleComponent updatedModuleComponent = moduleComponentGateway.save(moduleComponent);
-                        ModuleComponentDTO updatedModuleComponentDTO = modelMapper.map(updatedModuleComponent, ModuleComponentDTO.class);
-                        return new ResponseHttpDTO<>("200", "Componente actualizado correctamente", updatedModuleComponentDTO);
-                    }
-            ).orElseGet(() -> new ResponseHttpDTO<>("404", "El componente no existe", null));
-        } catch (DataAccessException e) {
-            return new ResponseHttpDTO<>("500",
-                    "Error al actualizar la información del componente: " + e.getMessage(), null);
-        } catch (Exception e) {
-            return new ResponseHttpDTO<>("500",
-                    "Ocurrió un error inesperado: " + e.getMessage(), null);
-        }
+        return crudService.updateResourceById(id, moduleComponentDTO, moduleComponentGateway, ModuleComponentDTO.class, "componente");
     }
 
     public ResponseHttpDTO<GroupPermissionDTO> assignComponentToGroup(RequestAssignAndRemoveBodyDTO body) {
